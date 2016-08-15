607908a806eb0ca7b9e377d2f204e3d99fa273b8
dlopes7 <davidribeirolopes@gmail.com>
2016-08-15 18:51:08 -0300

    Small improvement to tables.py

--- zabbix_admin/tables.py
+++ zabbix_admin/tables.py
@@ -1,16 +1,20 @@
 import django_tables2 as tables
-
 from zabbix_admin.models import Server
 
-class ServerTable(tables.Table):
 
+class ServerTable(tables.Table):
     instalar = tables.TemplateColumn(template_name='botao_instalar.html',
-                                    orderable=False,
+                                     orderable=False,
                                      verbose_name='Instalar')
 
     ignorar = tables.TemplateColumn(template_name='botao_ignorar.html',
                                     orderable=False,
-                                     verbose_name='Ignorar')
+                                    verbose_name='Ignorar')
+
+    host_id = tables.TemplateColumn(
+        '<a target="_blank" href="http://zabbix-server01.dc.nova/zabbix/items.php?filter_set=1&hostid={{record.host_id}}">{{record.host_id|default_if_none:""}}</a>',
+    verbose_name="Zabbix")
+
 
     class Meta:
         model = Server
